9dc31454024efa16b3e34344b08ee19595dade6c
Randy Wind <randywind@live.nl>
2018-09-30 13:40:03 +0200

    Generator begin

---
+++ ProbeMovementOptimizer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "NDevTypes.h"
+#include "NDevData.h"
+#include "NDevMathDistributions.h"
+#include "PMOShape.h"
+
+namespace ProbeMovementOptimizer
+{
+	using namespace NDev;
+	using namespace NDev::Types;
+
+	template<FSize SizePoint>
+	FVoid Generate(TShape<SizePoint> &Item, FSize K = 32, FSize Seed = 0)
+	{
+		using FShape = TShape<SizePoint>;
+		TNormal<FReal> Distribution;
+
+	}
+
+	template<FSize SizePoint>
+	FVoid Generate(TData<TShape<SizePoint>> &List, FSize N = 8, FSize K = 32, FSize Seed = 0)
+	{
+		List.Reserve(N, True);
+		for (auto &Item : List)
+		{
+			Generate(Item, K, Seed);
+		}
+	}
+}
